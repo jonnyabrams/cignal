17abaca58ff9a75c51f01068e3126e7cd7cd3df4
jonnyabrams <jonnyabrams@hotmail.com>
2022-12-14 08:24:18 +0000

    feat: navigation + navigation container

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
   return (
-    <View className='items-center justify-center flex-1'>
-      <StatusBar style="auto" />
-      <Text className="text-red-500">Oi oi oioioi</Text>
-    </View>
+    <NavigationContainer>
+      <View className="items-center justify-center flex-1">
+        <StatusBar style="auto" />
+        <Text className="text-red-500">Oi oi oioioi</Text>
+      </View>
+    </NavigationContainer>
   );
 }
-

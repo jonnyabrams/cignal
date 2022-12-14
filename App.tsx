c01a0f65ea0d800e7efbc62dd28853d1ae45fc7b
jonnyabrams <jonnyabrams@hotmail.com>
2022-12-14 08:44:18 +0000

    styles: global status bar styles

--- App.tsx
+++ App.tsx
@@ -1,21 +1,26 @@
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import LoginScreen from "./src/screens/LoginScreen";
 
 const Stack = createNativeStackNavigator();
 
+const globalScreenOptions = {
+  headerStyle: { backgroundColor: "#2C6BED" },
+  headerTitleStyle: { color: "white" },
+  headerTintColor: "white",
+};
+
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator screenOptions={globalScreenOptions}>
         <Stack.Screen name="Login" component={LoginScreen} />
       </Stack.Navigator>
-
     </NavigationContainer>
   );
-}
+};
 
-export default App
+export default App;

f50369b11d20f58542034f713805515cdc65a9e2
jonnyabrams <jonnyabrams@hotmail.com>
2022-12-14 08:53:02 +0000

    styles: LoginScreen status bar light theme

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,9 +1,11 @@
 import { View, Text } from "react-native";
 import { Button, Input, Image } from "@rneui/themed";
+import { StatusBar } from "expo-status-bar";
 
 const LoginScreen = () => {
   return (
     <View>
+      <StatusBar style="light" />
       <Text>LoginScreen</Text>
     </View>
   );

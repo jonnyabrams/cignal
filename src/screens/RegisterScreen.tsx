e31ca0f4e1849f7e94c5d54027160c003581d7a2
jonnyabrams <jonnyabrams@hotmail.com>
2022-12-17 08:57:45 +0000

    feat: scaffold RegisterScreen

--- src/screens/RegisterScreen.tsx
+++ src/screens/RegisterScreen.tsx
@@ -1,5 +1,9 @@
-import { StyleSheet, Text, View } from "react-native";
+import { KeyboardAvoidingView, StyleSheet, View } from "react-native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { Text, Button, Input } from "@rneui/themed";
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+
 import { RootStackParamList } from "../../App";
 
 type RegisterScreenNavigationProp = NativeStackNavigationProp<
@@ -12,13 +16,70 @@ type Props = {
 };
 
 const RegisterScreen = ({ navigation }: Props) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [passwordsMatch, setPasswordsMatch] = useState(true)
+
+  const register = () => {
+
+  }
+
   return (
-    <View>
-      <Text>RegisterScreen</Text>
-    </View>
+    <KeyboardAvoidingView behavior="padding" style={styles.container}>
+      <StatusBar style="light" />
+      <Text h3 style={{ marginBottom: 50 }}>
+        Create a Cignal account
+      </Text>
+
+      <View style={styles.inputContainer}>
+        <Input
+          placeholder="Full name"
+          autoFocus
+          value={name}
+          onChangeText={(text) => setName(text)}
+        />
+        <Input
+          placeholder="Email"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <Input
+          placeholder="Password"
+          value={password}
+          secureTextEntry
+          onChangeText={(text) => setPassword(text)}
+        />
+        <Input
+          placeholder="Confirm password"
+          value={confirmPassword}
+          secureTextEntry
+          onChangeText={(text) => setConfirmPassword(text)}
+        />
+        <Input
+          placeholder="Profile picture URL (optional)"
+          value={imageUrl}
+          onChangeText={(text) => setImageUrl(text)}
+          onSubmitEditing={register}
+        />
+      </View>
+
+      <Button
+        style={styles.button}
+        raised
+        onPress={register}
+        title="Register"
+      />
+    </KeyboardAvoidingView>
   );
 };
 
 export default RegisterScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {},
+  inputContainer: {},
+  button: {}
+});

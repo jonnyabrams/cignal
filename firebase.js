20b78ab16bb3adf0361e16ce2d4eff0ede5a4466
jonnyabrams <jonnyabrams@hotmail.com>
2022-12-18 18:14:57 +0000

    fix: downgrade to firebase 8

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
-import firebase from "firebase/compat/app";
-import "firebase/compat/auth";
-import "firebase/compat/firestore";
+import firebase from 'firebase/app';
+import "firebase/firestore";
+import "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDLHN1GFe2HCFSlHEev0cO6HfZ503JXlP4",
